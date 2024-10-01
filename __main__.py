ab8c4a4ccd6f1181d33585d6bed0f1880b8eb870
CleverLemming1337 <119337770+CleverLemming1337@users.noreply.github.com>
2024-10-01 09:55:01 +0000

    Update output formatting

--- __main__.py
+++ __main__.py
@@ -32,7 +32,7 @@ COLORS = {
 def print_nuclide(nuclide: dict) -> str:
     color = COLORS[nuclide['mode']] if nuclide['mode'] in COLORS else "9"
 
-    result = f"\033[1;3{color}m"
+    result = f"\033[9{color}m"
     
     result += f"{nuclide['name']:<2} {nuclide['m']:>3} {nuclide['z']:>3} {nuclide['mode']:<5}" + (f"{nuclide['life']:30,.10f}" if nuclide['life'] else "")+"\033[0m"
     return result
